66808fbd360b26a75efd3d06c8be9f866d3b89c2
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2024-01-06 09:25:19 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("DabangTheBrand")
+                await self.one.join_chat("DabangTheBrand")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.two.join_chat("DabangTheBrand")
+                await self.one.join_chat("DabangTheBrand")
             except:
                 pass
             assistants.append(2)
